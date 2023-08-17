9704767ba90eb467f22d8ead1b8268ec3e907ccf
NavalPandey214 <131510964+NavalPandey214@users.noreply.github.com>
2023-08-17 09:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// your code here
+
+ // Get references to the form, input fields, and URL element
+    const form = document.getElementById("myForm");
+    const nameInput = document.getElementById("name"); 
+    const yearInput = document.getElementById("year");
+    const urlElement = document.getElementById("url");
+
+    // Add event listener to the form submit event
+    form.addEventListener("submit", (event) => {
+      event.preventDefault(); // Prevent form submission
+
+      // Get the values from the input fields
+      const name = nameInput.value;
+      const year = yearInput.value;
+
+      // Construct the URL query string
+      const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+
+      // Update the URL element with the generated URL
+      urlElement.textContent = `https://localhost:8080/${queryString}`;
+    });
